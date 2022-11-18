79356f2f5ae6f156fe2201b8ff2778f382e8205d
Jamil khan <jamilkhan@Jamils-MacBook-Pro.local>
2022-11-18 07:39:30 +0530

    hey

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.post("/create",(req,res)=>{
-    res.send("hey vakeel khan")
+    res.send("hey vakeel khan mai")
 })
 app.listen(process.env.PORT ||3000,(req,res)=>{
     console.log("server is run on 3000 port");
