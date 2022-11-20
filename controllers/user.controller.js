57494d1f7c7e6c0c1dba91d3859c9c7a5a45666f
Jamil khan <jamilkhan@Jamils-MacBook-Pro.local>
2022-11-20 07:55:00 +0530

    ghjk

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,49 +1,49 @@
-import user from "../moduls/user.modul.js";
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-export const signup = async (req,res)=>{
-     try {
-        const isemailexits = await user.findOne({email:req.body.email})
-        const isnumberexits = await user.findOne({number:req.body.number})
-        if(isemailexits){
-            res.send({
-                status:false,
-                msg:"email is already exits .",
-                data:{}
-            })
-        }else if(isnumberexits){
-            res.send({
-                status:false,
-                msg:"number is already exits .",
-                data:{}
-            })
-        }else{
-            var otp = Math.floor(1000 + Math.random() * 9000);
-            req.body.otp =otp
-            var passhash = await bcrypt.hash(req.body.password,10)
-            req.body.password = passhash
-            const create = await user.create(req.body)
-            if(create){
-                create.token = await jwt.sign({time:Date(),reateId:create.id},"deshwali")
-                res.send({
-                    status:true,
-                    msg:"signup successfully",
-                    data:create
-                })
-            }
-            else{
-                res.send({
-                    status:false,
-                    msg:" is not create data",
-                    data:{}
-                })
-            }
-        }
-     } catch (err) {
-        res.send({
-            status:true,
-            msg:"something wrong with request .",
-            data:{}
-        })
-     }
-}
+// import user from "../moduls/user.modul.js";
+// import bcrypt from "bcrypt"
+// import jwt from "jsonwebtoken"
+// export const signup = async (req,res)=>{
+//      try {
+//         const isemailexits = await user.findOne({email:req.body.email})
+//         const isnumberexits = await user.findOne({number:req.body.number})
+//         if(isemailexits){
+//             res.send({
+//                 status:false,
+//                 msg:"email is already exits .",
+//                 data:{}
+//             })
+//         }else if(isnumberexits){
+//             res.send({
+//                 status:false,
+//                 msg:"number is already exits .",
+//                 data:{}
+//             })
+//         }else{
+//             var otp = Math.floor(1000 + Math.random() * 9000);
+//             req.body.otp =otp
+//             var passhash = await bcrypt.hash(req.body.password,10)
+//             req.body.password = passhash
+//             const create = await user.create(req.body)
+//             if(create){
+//                 create.token = await jwt.sign({time:Date(),reateId:create.id},"deshwali")
+//                 res.send({
+//                     status:true,
+//                     msg:"signup successfully",
+//                     data:create
+//                 })
+//             }
+//             else{
+//                 res.send({
+//                     status:false,
+//                     msg:" is not create data",
+//                     data:{}
+//                 })
+//             }
+//         }
+//      } catch (err) {
+//         res.send({
+//             status:true,
+//             msg:"something wrong with request .",
+//             data:{}
+//         })
+//      }
+// }
--- moduls/user.modul.js
+++ moduls/user.modul.js
@@ -1,53 +1,53 @@
-import mongoose from "mongoose";
-const userschema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    number: {
-        type: Number,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: false
-    },
-    createdAt:
-    {
-        type: Date,
-        default: Date.now
-    },
-    updatedAt:
-    {
-        type: Date,
-        default: Date.now
-    },
-    toke:{
-        type: String,
-        required: false
-    },
-    otp:{
-        type: Number,
-        required: false
-    },
-    email_verified:{
-        type:Boolean,
-        required:false,
-        default:false
-    },
-   number_verified:{
-        type:Boolean,
-        required:false,
-        default:false
-    }
-})
-const  user = mongoose.model("users",userschema)
-export default user
+// import mongoose from "mongoose";
+// const userschema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true
+//     },
+//     number: {
+//         type: Number,
+//         required: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     },
+//     age: {
+//         type: Number,
+//         required: false
+//     },
+//     createdAt:
+//     {
+//         type: Date,
+//         default: Date.now
+//     },
+//     updatedAt:
+//     {
+//         type: Date,
+//         default: Date.now
+//     },
+//     toke:{
+//         type: String,
+//         required: false
+//     },
+//     otp:{
+//         type: Number,
+//         required: false
+//     },
+//     email_verified:{
+//         type:Boolean,
+//         required:false,
+//         default:false
+//     },
+//    number_verified:{
+//         type:Boolean,
+//         required:false,
+//         default:false
+//     }
+// })
+// const  user = mongoose.model("users",userschema)
+// export default user
