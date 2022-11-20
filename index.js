ca3785b606f1a934a0b6c123bd77ec7a1d45c455
Jamil khan <jamilkhan@Jamils-MacBook-Pro.local>
2022-11-20 09:51:11 +0530

    hahha

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// import express from "express";
-// import category from "./routes/category.route.js";
-// import connectDB from "./config/db.js";
-// import product from "./routes/product.route.js";
-// // import user from "./routes/user.route.js"
-// const app = express();
-// app.use(express.json());
-// connectDB();
-// app.use(product)
-// app.use(category)
-// // app.use(user)
-// app.listen(process.env.PORT || 3000, (req, res) => {
-//     console.log("server is run on 3000 port");
-// })
+import express from "express";
+import category from "./routes/category.route.js";
+import connectDB from "./config/db.js";
+import product from "./routes/product.route.js";
+// import user from "./routes/user.route.js"
+const app = express();
+app.use(express.json());
+connectDB();
+app.use(product)
+app.use(category)
+// app.use(user)
+app.listen(process.env.PORT || 3000, (req, res) => {
+    console.log("server is run on 3000 port");
+})
--- routes/user.route.js
+++
@@ -1,6 +0,0 @@
-// import { signup } from "../controllers/user.controller.js";
-// import  Express  from "express";
-// const user = Express.Router();
-// user.route("/user/signup").post(signup)
-// export default user
- 
