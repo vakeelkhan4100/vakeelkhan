f41b25d65c4f1a7d4b65b7cb23ab6f9845ec364e
Jamil khan <jamilkhan@Jamils-MacBook-Pro.local>
2022-12-03 13:52:30 +0530

    all

--- index.js
+++ index.js
@@ -10,40 +10,40 @@ import share from "./routes/share.route.js";
 import  {like}  from "./routes/like.route.js";
 const app = express();
 app.use(express.json());
-// connectDB();
-import { createServer } from "http";
-import { Server } from "socket.io";
-const httpServer = createServer();
-const socket = new Server(httpServer, {
-	cors: {
-		origins: ['http://dev.adaiya.in','http://localhost:4200','http://localhost:9002'],
-		'transports': ['websocket', 'pollings']
-	  },
-});
+connectDB();
+// import { createServer } from "http";
+// import { Server } from "socket.io";
+// const httpServer = createServer();
+// const socket = new Server(httpServer, {
+// 	cors: {
+// 		origins: ['http://dev.adaiya.in','http://localhost:4200','http://localhost:9002'],
+// 		'transports': ['websocket', 'pollings']
+// 	  },
+// });
 
-var users = {};
-socket.on('connection', (socket) => {
-    console.log("Connected...");
-	socket.on('judna', function(data){
-		console.log("Connected Join----",data);
-	  socket.join(data.room);
-	  users[socket.id] = data.user;
-	  var  JoinRes = {
-		message: data.user+' has joined '+data.room+" room",
-		users:users,
-	};
-	  socket.broadcast.to(data.room).emit('ha_jud_gya_hai', JoinRes);
-	});
-});
+// var users = {};
+// socket.on('connection', (socket) => {
+//     console.log("Connected...");
+// 	socket.on('judna', function(data){
+// 		console.log("Connected Join----",data);
+// 	  socket.join(data.room);
+// 	  users[socket.id] = data.user;
+// 	  var  JoinRes = {
+// 		message: data.user+' has joined '+data.room+" room",
+// 		users:users,
+// 	};
+// 	  socket.broadcast.to(data.room).emit('ha_jud_gya_hai', JoinRes);
+// 	});
+// });
  
-// app.use(product)
-// app.use(category)
-// app.use(reviewrating)
-// app.use(subcategory)
-// app.use(user)
-// app.use(comment)
-// app.use(share)
-// app.use(like)
-httpServer.listen(process.env.PORT || 3000, (req, res) => {
+app.use(product)
+app.use(category)
+app.use(reviewrating)
+app.use(subcategory)
+app.use(user)
+app.use(comment)
+app.use(share)
+app.use(like)
+app.listen(process.env.PORT || 3000, (req, res) => {
     console.log("server is run on 3000 port");
 })
